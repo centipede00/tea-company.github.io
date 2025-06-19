1728f88c206f46638a9452f62e31ffc620caea93
centipede00 <matvienkoyula2005@gmail.com>
2025-06-19 19:06:56 +0900

    Delete tea-company directory

--- tea-company/index.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    document.getElementById('response').textContent = 'Спасибо за ваше сообщение! Мы свяжемся с вами в ближайшее время.';
-  });
-  
